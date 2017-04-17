068d776e5470ef39ab51bc751894d2fad1e3650a
Andrew Lee <fiacre.patrick@gmail.com>
2017-04-17 17:43:02 -0400

    Add name field
    Add name field to BlackList and BlackListObject models

--- src/oscarbluelight/offer/models.py
+++ src/oscarbluelight/offer/models.py
@@ -177,10 +177,12 @@ class BlackList(models.Model):
     can not be combined
     Need only know the id of the offer obj
     attributes:
+    name -- name of this black list
     classname -- name of class (offer or voucher)
     instance_id -- PK of offer or voucher obj
     blacklist -- M2M field of BlackListObject
     """
+    name = models.CharField(max_length=128, null=False, blank=True)
     classname = models.CharField(max_length=128, null=False, blank=True)
     instance_id = models.PositiveIntegerField(null=False)
     blacklist = models.ManyToManyField(BlackListObject, related_name='blackisted_objects')
@@ -204,6 +206,10 @@ class BlackList(models.Model):
 
 # Make proxy_class field not unique.
 Condition._meta.get_field('proxy_class')._unique = False
+
+
+class OfferGroup(models.Model):
+    name = 
 
 
 __all__ = [
