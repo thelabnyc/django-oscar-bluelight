c109ce9c72597dc06f450c7ede26432962d898e9
vmuraru <vmuraru@thelabnyc.com>
2017-09-30 20:58:28 -0400

    Fixed lint problems

--- server/src/oscarbluelight/tests/selenium/test_short_name.py
+++ server/src/oscarbluelight/tests/selenium/test_short_name.py
@@ -1,16 +1,14 @@
 from decimal import Decimal as D
 from unittest import skipIf
-from oscar.core.loading import get_model
+# from oscar.core.loading import get_model
 from .base import SplinterSeleniumTestCase
 from . import SKIP_SELENIUM_TESTS
-from selenium.webdriver.common.keys import Keys
-from oscarbluelight.offer.models import Condition, ConditionalOffer, Range, Benefit, OfferGroup
+# from selenium.webdriver.common.keys import Keys
+from oscarbluelight.offer.models import Condition, Range, Benefit
 
-import time
-
-Range = get_model('offer', 'Range')
-Condition = get_model('offer', 'Condition')
-ConditionalOffer = get_model('offer', 'ConditionalOffer')
+# Range = get_model('offer', 'Range')
+# Condition = get_model('offer', 'Condition')
+# ConditionalOffer = get_model('offer', 'ConditionalOffer')
 
 
 @skipIf(SKIP_SELENIUM_TESTS, "Skipping Selenium Tests")
@@ -19,7 +17,7 @@ class ShortNameDashboardTest(SplinterSeleniumTestCase):
         super().setUp()
 
     def test_short_name_saved(self):
-      
+
         # Create some defaults
         all_products = Range()
         all_products.includes_all_products = True
@@ -34,11 +32,11 @@ class ShortNameDashboardTest(SplinterSeleniumTestCase):
         benefit.proxy_class = 'oscarbluelight.offer.benefits.BluelightShippingFixedPriceBenefit'
         benefit.value = 0
         benefit.save()
-        
+
         # Create new offer
         self._visit_raw('/dashboard/offers/new/name-and-description/')
         self._take_screenshot('short-name-1')
-        
+
         # Fill in first step
         self.browser.fill('name', 'offer 1 name')
         self.browser.fill('short_name', 'offer 1 short name')
@@ -57,15 +55,15 @@ class ShortNameDashboardTest(SplinterSeleniumTestCase):
         self._take_screenshot('short-name-2')
 
         go_step2_btn.click()
-        self._take_screenshot('short-name-3')  
-        
+        self._take_screenshot('short-name-3')
+
         # Fill in second step
         self._select_fancy('benefit', 'Get shipping for $0.00')
         self._take_screenshot('short-name-4')
         go_step3_btn = self.browser.find_by_text('Continue to step 3').first
         go_step3_btn.click()
         self._take_screenshot('short-name-5')
-        
+
         # Fill in third step
         # Select condition
         # el1 = self.browser.find_by_xpath('//select[@name="condition"]/option[2]').first
@@ -75,13 +73,12 @@ class ShortNameDashboardTest(SplinterSeleniumTestCase):
         go_step4_btn = self.browser.find_by_text('Continue to step 4').first
         go_step4_btn.click()
         self._take_screenshot('short-name-7')
- 
+
         # Fill in fourth step
         save_offer_btn = self.browser.find_by_text('Save this offer').first
         save_offer_btn.click()
         self._take_screenshot('short-name-8')
-        
+
         # Verify the short name is there
         el = self.browser.find_by_xpath('//*[@id="default"]/div[1]/div/div[3]/table[2]/tbody/tr[2]')
         self.assertEqual('Short Name offer 1 short name', el.text)
-
