a8fed21981ecf1826b448463b7833bc14c897588
David Currie <davidalexandercurrie@gmail.com>
2023-09-21 11:54:05 -0400

    fix merge

--- server/src/oscarbluelight/offer/models.py
+++ server/src/oscarbluelight/offer/models.py
@@ -5,7 +5,6 @@ from django.core import exceptions
 from django.db import models, IntegrityError, connection
 from django.db.models import F
 from django.db.models import Q
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 from django.utils.encoding import force_str
 from django.utils.functional import cached_property
