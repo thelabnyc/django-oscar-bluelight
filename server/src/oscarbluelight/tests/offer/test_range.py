thelabnyc/django-oscar-bluelight

commit 585f83641e91aaddab41224441495c4f0b10bb6b

Refs #17498 Restore Range.add_product_batch method mistakenly deleted by merge conflict

server/src/oscarbluelight/offer/models.py
@@ -284,6 +284,22 @@ class Range(AbstractRange):
         # Query the cache view
         return Product.objects.all().filter(cached_ranges__range=self)
 
+    def add_product_batch(self, products):
+        """
+        Same as Range.add_product, but works on a batch of products (in order to optimize the number
+        of queries run on the DB)
+        """
+        # Insert new rows into the included_products relationship
+        RangeProduct = self.included_products.through
+        rows = [RangeProduct(range=self, product=product) for product in products]
+        RangeProduct.objects.bulk_create(rows, ignore_conflicts=True)
+        # Remove product from excluded products if it was removed earlier and
+        # re-added again, thus it returns back to the range product list.
+        ExcludedProduct = self.excluded_products.through
+        ExcludedProduct.objects.filter(product__in=products).all().delete()
+        # Invalidate cache because queryset has changed
+        self.invalidate_cached_queryset()
+
     def delete(self, *args, **kwargs):
         # Disallow deleting a range with any dependents
         if self.benefit_set.exists() or self.condition_set.exists():

server/src/oscarbluelight/tests/offer/test_range.py
@@ -221,3 +221,39 @@ class TestRangeModel(TestCase):
         first_range.name = "Bar"
         first_range.save()
         models.Range.objects.create(name="Foo")
+
+
+class TestRangeAddProductBatch(TestCase):
+    def test_add_product_batch(self):
+        rng = models.Range.objects.create(name="Range", includes_all_products=False)
+        product1 = create_product()
+        product2 = create_product()
+        product3 = create_product()
+        product4 = create_product()
+        # Add p2 to excluded products
+        rng.excluded_products.add(product2)
+        # Check starting point
+        self.assertFalse(rng.contains_product(product1))
+        self.assertFalse(rng.contains_product(product2))
+        self.assertFalse(rng.contains_product(product3))
+        self.assertFalse(rng.contains_product(product4))
+        # Add p1, p2, and p3 to the range. Should add all 3 products and remove p2 from the exclude list.
+        rng.add_product_batch(
+            [
+                product1,
+                product2,
+                product3,
+            ]
+        )
+        # Check results
+        self.assertTrue(rng.contains_product(product1))
+        self.assertTrue(rng.contains_product(product2))
+        self.assertTrue(rng.contains_product(product3))
+        self.assertFalse(rng.contains_product(product4))
+        # Add p2 back to excluded products
+        rng.excluded_products.add(product2)
+        # Check results
+        self.assertTrue(rng.contains_product(product1))
+        self.assertFalse(rng.contains_product(product2))
+        self.assertTrue(rng.contains_product(product3))
+        self.assertFalse(rng.contains_product(product4))

commit 523e5da7c375b36537f407e57516039a0bb1e7f0

fix: fix behavior of bulk add/exclude forms in RangeProductListView
Fixes two related bugs:

1. Using the bulk SKU/UPC text field to _exclude_ products from a range would actually add those products to the range.
2. Add a product to a range would additionally remove it from the exclusion list of ALL ranges.

Bug 1 seems to have been introduced upstream by [1d55f8df](https://github.com/django-oscar/django-oscar/commit/1d55f8df07628601dc803b3f243da212a577d814)

Bug 2 seems to have been introduced in 585f83641e91aaddab41224441495c4f0b10bb6b and gone unnoticed since then.

server/src/oscarbluelight/dashboard/ranges/views.py
@@ -15,6 +15,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 Range = get_model("offer", "Range")
+RangeProductFileUpload = get_model("offer", "RangeProductFileUpload")
 BatchPriceUpdateForm = get_class("ranges_dashboard.forms", "BatchPriceUpdateForm")
 RangeExcludedProductsUpdateForm = get_class(
     "ranges_dashboard.forms", "RangeExcludedProductsUpdateForm"
@@ -142,30 +143,43 @@ class RangeProductListView(BaseRangeProductListView):
 
         return products
 
-    def handle_query_products(self, request, range, form):
+    def handle_query_products(self, request, product_range, form):
         products = form.get_products()
         if not products:
             return
-        # Add the products to the range in a batch
-        range.add_product_batch(products)
+        # Check what kind of operaiton this is, add or exclude
+        if (
+            form.cleaned_data["upload_type"]
+            == RangeProductFileUpload.EXCLUDED_PRODUCTS_TYPE
+        ):
+            product_range.exclude_product_batch(products)
+            action = _("excluded from this range")
+        else:
+            # Add the products to the range in a batch
+            product_range.add_product_batch(products)
+            action = _("added to this range")
         # Message the user
         num_products = len(products)
         messages.success(
             request,
             ngettext(
-                "%d product added to range", "%d products added to range", num_products
+                "%(num_products)d product has been %(action)s",
+                "%(num_products)d products have been %(action)s",
+                num_products,
             )
-            % num_products,
+            % {"num_products": num_products, "action": action},
         )
         dupe_skus = form.get_duplicate_skus()
         if dupe_skus:
             messages.warning(
                 request,
                 _(
-                    "The products with SKUs or UPCs matching %s are already in this range"
+                    "The products with SKUs or UPCs matching %(skus)s have "
+                    "already been %(action)s"
                 )
-                % ", ".join(dupe_skus),
+                % {"skus": ", ".join(dupe_skus), "action": action},
             )
+
         missing_skus = form.get_missing_skus()
         if missing_skus:
             messages.warning(

server/src/oscarbluelight/offer/models.py
@@ -367,7 +367,26 @@ class Range(AbstractRange):
         # Remove product from excluded products if it was removed earlier and
         # re-added again, thus it returns back to the range product list.
         ExcludedProduct = self.excluded_products.through
-        ExcludedProduct.objects.filter(product__in=products).all().delete()
+        ExcludedProduct.objects.filter(range=self, product__in=products).all().delete()
+        # Queue a view refresh
+        queue_rps_view_refresh()
+        # Invalidate cache because queryset has changed
+        self.invalidate_cached_queryset()
+
+    def exclude_product_batch(self, products):
+        """
+        Inverse of add_product_batch
+        """
+        from .handlers import queue_rps_view_refresh
+
+        # Insert new rows into the excluded_products relationship
+        ExcludedProduct = self.excluded_products.through
+        rows = [ExcludedProduct(range=self, product=product) for product in products]
+        ExcludedProduct.objects.bulk_create(rows, ignore_conflicts=True)
+        # Remove product from excluded products if it was removed earlier and
+        # re-added again, thus it returns back to the range product list.
+        RangeProduct = self.included_products.through
+        RangeProduct.objects.filter(range=self, product__in=products).all().delete()
         # Queue a view refresh
         queue_rps_view_refresh()
         # Invalidate cache because queryset has changed

server/src/oscarbluelight/tests/offer/test_range.py
@@ -225,40 +225,105 @@ class TestRangeModel(TransactionTestCase):
 
 class TestRangeAddProductBatch(TransactionTestCase):
     def test_add_product_batch(self):
-        rng = models.Range.objects.create(name="Range", includes_all_products=False)
+        # Create some products
         product1 = create_product()
         product2 = create_product()
         product3 = create_product()
         product4 = create_product()
-        # Add p2 to excluded products
-        rng.excluded_products.add(product2)
 
-        # Check starting point
-        self.assertFalse(rng.contains_product(product1))
-        self.assertFalse(rng.contains_product(product2))
-        self.assertFalse(rng.contains_product(product3))
-        self.assertFalse(rng.contains_product(product4))
+        # Crwate two ranges. Range 2 includes all products by default.
+        rng1 = models.Range.objects.create(name="Range 1", includes_all_products=False)
+        rng2 = models.Range.objects.create(name="Range 2", includes_all_products=True)
 
-        # Add p1, p2, and p3 to the range. Should add all 3 products and remove p2 from the exclude list.
-        rng.add_product_batch(
-            [
-                product1,
-                product2,
-                product3,
-            ]
-        )
+        # Exclude p2 from both ranges
+        rng1.excluded_products.add(product2)
+        rng2.excluded_products.add(product2)
 
-        # Check results
-        self.assertTrue(rng.contains_product(product1))
-        self.assertTrue(rng.contains_product(product2))
-        self.assertTrue(rng.contains_product(product3))
-        self.assertFalse(rng.contains_product(product4))
+        # Check starting point. Range 1 contains nothing. Range 2 contains
+        # everything except p2.
+        self.assertFalse(rng1.contains_product(product1))
+        self.assertFalse(rng1.contains_product(product2))
+        self.assertFalse(rng1.contains_product(product3))
+        self.assertFalse(rng1.contains_product(product4))
+
+        self.assertTrue(rng2.contains_product(product1))
+        self.assertFalse(rng2.contains_product(product2))
+        self.assertTrue(rng2.contains_product(product3))
+        self.assertTrue(rng2.contains_product(product4))
+
+        # Add p1, p2, and p3 to range 1. Should add all 3 products and remove
+        # p2 from the exclude list. Should _not_ affect range 2.
+        rng1.add_product_batch([product1, product2, product3])
+
+        # Check results. Range 1 should now include p{1,2,3}. Range 2 should
+        # include everything except for p2.
+        self.assertTrue(rng1.contains_product(product1))
+        self.assertTrue(rng1.contains_product(product2))
+        self.assertTrue(rng1.contains_product(product3))
+        self.assertFalse(rng1.contains_product(product4))
+
+        self.assertTrue(rng2.contains_product(product1))
+        self.assertFalse(rng2.contains_product(product2))
+        self.assertTrue(rng2.contains_product(product3))
+        self.assertTrue(rng2.contains_product(product4))
 
         # Add p2 back to excluded products
-        rng.excluded_products.add(product2)
+        rng1.excluded_products.add(product2)
 
         # Check results
-        self.assertTrue(rng.contains_product(product1))
-        self.assertFalse(rng.contains_product(product2))
-        self.assertTrue(rng.contains_product(product3))
-        self.assertFalse(rng.contains_product(product4))
+        self.assertTrue(rng1.contains_product(product1))
+        self.assertFalse(rng1.contains_product(product2))
+        self.assertTrue(rng1.contains_product(product3))
+        self.assertFalse(rng1.contains_product(product4))
+
+        self.assertTrue(rng2.contains_product(product1))
+        self.assertFalse(rng2.contains_product(product2))
+        self.assertTrue(rng2.contains_product(product3))
+        self.assertTrue(rng2.contains_product(product4))
+
+
+class TestRangeExcludeProductBatch(TransactionTestCase):
+    def test_exclude_product_batch(self):
+        # Create some products
+        product1 = create_product()
+        product2 = create_product()
+        product3 = create_product()
+        product4 = create_product()
+
+        # Create two ranges.
+        rng1 = models.Range.objects.create(name="Range 1", includes_all_products=False)
+        rng2 = models.Range.objects.create(name="Range 2", includes_all_products=False)
+
+        # Add p{1,2,3} to both ranges
+        rng1.included_products.add(product1)
+        rng1.included_products.add(product2)
+        rng1.included_products.add(product3)
+        rng2.included_products.add(product1)
+        rng2.included_products.add(product2)
+        rng2.included_products.add(product3)
+
+        # Check starting point. Both ranges contain p{1,2,3}
+        self.assertTrue(rng1.contains_product(product1))
+        self.assertTrue(rng1.contains_product(product2))
+        self.assertTrue(rng1.contains_product(product3))
+        self.assertFalse(rng1.contains_product(product4))
+
+        self.assertTrue(rng2.contains_product(product1))
+        self.assertTrue(rng2.contains_product(product2))
+        self.assertTrue(rng2.contains_product(product3))
+        self.assertFalse(rng2.contains_product(product4))
+
+        # Exclude p{1,2,4} from range 1. Should _not_ affect range 2.
+        rng1.exclude_product_batch([product1, product2, product4])
+
+        # Check results. Range 1 should now include p{1,2,3}. Range 2 should
+        # include everything except for p2.
+        self.assertFalse(rng1.contains_product(product1))
+        self.assertFalse(rng1.contains_product(product2))
+        self.assertTrue(rng1.contains_product(product3))
+        self.assertFalse(rng1.contains_product(product4))
+
+        self.assertTrue(rng2.contains_product(product1))
+        self.assertTrue(rng2.contains_product(product2))
+        self.assertTrue(rng2.contains_product(product3))
+        self.assertFalse(rng2.contains_product(product4))